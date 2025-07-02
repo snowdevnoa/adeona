c5441b2d4386e0f051aee1cbfec52261e7bdb9c2
Noa <snowdevnoa@gmail.com>
2025-07-02 10:16:32 -0500

    Add client-side middleware to reroute authenticated users away from login and registration pages

---
+++ frontend/src/middleware.js
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+	if (request.cookies.has("access_token")) {
+		console.log("User is already logged in");
+		return NextResponse.redirect(new URL("/", request.url));
+	} else {
+		return NextResponse.next();
+	}
+}
+
+export const config = {
+	matcher: ["/login", "/register"],
+};
